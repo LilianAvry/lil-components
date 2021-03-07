2dfbb3b47366ea94e3e32cfe05ddf9a1bf1a3bad
lilianchretienne <lilian.chretienne@erplain.com>
2021-03-07 20:09:42 +0100

    Add build script

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const lilPath = `${__dirname}/src/lil-components.css`;
+
+fs.writeFileSync(lilPath, '');
+
+function read (name, index) {
+  const path = `${__dirname}/src/${name.toLowerCase()}/style.css`;
+  const options = { encoding:'utf8', flag:'r' };
+  const content = fs.readFileSync(path, options);
+  const comment = index === 0
+    ? `/* ${name} -------------------------------- */\r\n`
+    : `\r\n/* ${name} -------------------------------- */\r\n`;
+  const data = comment + content;
+  fs.appendFileSync(lilPath, data);
+}
+
+const components = ['Info', 'Social'];
+
+for (let i = 0; i < components.length; i++) {
+  read(components[i], i)
+}
